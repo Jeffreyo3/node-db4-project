1ee8c3cebe474acd45badf7ef89a8259429fca48
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2020-02-06 20:17:36 -0800

    Update migration

--- migrations/20200206140111_create-tables.js
+++ migrations/20200206140111_create-tables.js
@@ -1,21 +1,41 @@
 
 exports.up = function (knex) {
     // 1 recipe has many ingredients
-    return knex.schema.createTable('recipes', tbl => {
-        tbl.increments();
-        tbl.string('recipe_name', 128).notNullable();
-    })
+    return knex.schema
+        .createTable('recipes', tbl => {
+            tbl.increments();
+            tbl.string('recipe_name', 128).notNullable();
+        })
         .createTable('ingredients', tbl => {
             tbl.increments();
             tbl.string('ingredient_name', 128).notNullable();
             // foreign key that points to recipes table
-            tbl.integer('ingredient_id')
+            tbl.integer('recipe_id')
                 .unsigned()
                 .notNullable()
                 .references('id')
                 .inTable('recipes');
         })
+        .createTable('steps', tbl => {
+            tbl.increments();
+            tbl.string('instructions').notNullable();
+            // foreign key that points to recipes table
+            tbl.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('recipes');
+            // foreign key that points to ingredients table
+            tbl.integer('ingredient_id')
+                .unsigned()
+                .references('id')
+                .inTable('incredients');
+        })
+
+
 };
+
+
 
 exports.down = function (knex) {
     return knex.schema
