0d9c273fd52d40e85d002a3d8ced7ecd9a27a2ed
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2020-02-06 19:37:07 -0800

    Create notes.md to draft relationships and possible data points needed for tables

--- migrations/20200206140111_create-tables.js
+++ migrations/20200206140111_create-tables.js
@@ -1,23 +1,24 @@
 
-exports.up = function(knex) {
-  // 1 recipe has many ingredients
-  return knex.schema.createTable('recipes', tbl => {
-      tbl.increments();
-      tbl.string('recipe_name', 128).notNullable();
-  })
-  .createTable('ingredients', tbl => {
-      tbl.increments();
-      tbl.string('ingredient_name', 128).notNullable();
-      // foreign key that points to recipes table
-      tbl.integer('ingredient_id')
-        .unsigned()
-        .notNullable()
-        .references('id')
-        .inTable('recipes');
-  })
+exports.up = function (knex) {
+    // 1 recipe has many ingredients
+    return knex.schema.createTable('recipes', tbl => {
+        tbl.increments();
+        tbl.string('recipe_name', 128).notNullable();
+    })
+        .createTable('ingredients', tbl => {
+            tbl.increments();
+            tbl.string('ingredient_name', 128).notNullable();
+            // foreign key that points to recipes table
+            tbl.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('recipes');
+        })
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('ingredients')
-    .dropTableIfExists('recipes');
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('recipes');
 };
---
+++ notes.md
@@ -0,0 +1,39 @@
+# Notes
+
+## Needed Data
+Recipes
+Ingredients
+Ingredients in a recipe
+Quantity of ingredients in a recipe
+Steps in a recipe
+
+### Possible Tables
+recipes
+    id
+    recipe_name
+
+ingredients
+    ingredient_id
+    ingredient_name
+
+steps
+    step_id
+    instructions
+    recipe_id
+    step_number
+
+recipe_ingredients
+    recipe_id
+    ingredient_id
+    ingredient_quantity
+
+### Table Relationships
+
+Recipes (one) <==> Steps (many)
+
+Ingredients (many) <==> Steps (many)
+
+Ingredients (many) <==> Recipes
+
+
+
